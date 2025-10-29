a97fe2372de017ea9e36e8791dfff6b3f773d272
Sureshtr1998 <suresh@stepchange.earth>
2025-10-29 21:18:29 +0530

    deployment

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ mongoose
   .then(() => console.log("MongoDB connected on", process.env.MONGO_URI))
   .catch((err) => console.error(err));
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app;
