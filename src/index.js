8e5e7ef3eafb0821f975beaa64b1fa491210169b
Sureshtr1998 <suresh@stepchange.earth>
2025-11-07 19:44:05 +0530

    CORS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
 
-app.use(express.json());
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use("/api", authRoutes);
@@ -12,6 +21,7 @@ mongoose
   .then(() => console.log("MongoDB connected on", process.env.MONGO_URI))
   .catch((err) => console.error(err));
 
+// Start server
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
